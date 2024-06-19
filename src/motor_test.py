3020873981673eba591b48944d85a9a401dc653c
Walker Allen <walker@a3z.co>
2024-06-19 18:11:26 -0400

    minor changes

--- src/motor_test.py
+++ src/motor_test.py
@@ -10,9 +10,9 @@ mymotortest = RpiMotorLib.A4988Nema(direction, step, (21, 21, 21), "DRV8825")
 GPIO.setup(EN_pin, GPIO.OUT)
 
 GPIO.output(EN_pin, GPIO.LOW)
-mymotortest.motor_go(False, "1/16", 6400, 0.0000625, False, 0.05)
+mymotortest.motor_go(False, "1/16", 12800, 0.00003125, False, 0.05)
 
 GPIO.output(EN_pin, GPIO.LOW)
-mymotortest.motor_go(True, "1/16", 6400, 0.0000625, False, 0.05)
+mymotortest.motor_go(True, "1/16", 12800, 0.00003125, False, 0.05)
 
 GPIO.cleanup()

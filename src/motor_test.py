ff872be0fe95594950ede6a7a87938aebd0d8fb6
Walker Allen <walker@a3z.co>
2024-06-19 16:45:58 -0400

    changed delay

--- src/motor_test.py
+++ src/motor_test.py
@@ -10,6 +10,6 @@ mymotortest = RpiMotorLib.A4988Nema(direction, step, (21, 21, 21), "DRV8825")
 GPIO.setup(EN_pin, GPIO.OUT)
 
 GPIO.output(EN_pin, GPIO.LOW)
-mymotortest.motor_go(False, "Half", 200, 0.0005, False, 0.05)
+mymotortest.motor_go(False, "Full", 200, 0.005, False, 0.05)
 
 GPIO.cleanup()

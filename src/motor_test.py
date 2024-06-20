833baf7815a52ec8809107bc7f5ffdf5c793fefc
Walker Allen <walker@a3z.co>
2024-06-20 14:44:19 -0400

    nothing really

--- src/motor_test.py
+++ src/motor_test.py
@@ -1,11 +1,3 @@
-#import motor_controls
-import math
-
-#motor_controls.auto_run(25600, 0.000003, 0.05, 2)
-
-def round_up(number, decimals):
-    multiplier = 10 ** decimals
-    return math.ceil(number * multiplier) / multiplier
+import motor_controls
 
 
-print(round_up((1 / (5 * 2 / (0.003175 / 16))), 8))

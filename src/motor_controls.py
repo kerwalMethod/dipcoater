683a08c3a6b07bc1a09f8c42d5e84808a914969a
Walker Allen <walker@a3z.co>
2024-06-19 20:11:32 -0400

    Created new motor control file and updated motor test to use it

--- src/motor_controls.py
+++ src/motor_controls.py
@@ -7,6 +7,66 @@ direction_pin = 22
 step_pin = 23
 enable_pin = 24
 
-
-
+# Create a boolean variable for stopping the motor
 stop_motor = False
+
+# Set how you are referring to GPIO pins (BCM = Broadcom SOC Channel)
+GPIO.setmode(GPIO.BCM)
+
+# Disable GPIO warnings
+GPIO.setwarnings(False)
+
+
+# Create a function to stop the motor
+def motor_stop():
+
+    # Set the boolean variable to true to the motor will stop
+    stop_motor = True
+
+
+# Create a function for automated runs
+def auto_run(direction, steps, stepdelay, initialdelay):
+
+    # Set the boolean variable to false so the motor can run
+    stop_motor = False
+
+    # Setup the enable GPIO pin
+    GPIO.setup(enable_pin, GPIO.OUT)
+    GPIO.output(enable_pin, GPIO.LOW)
+
+    # Setup the direction and step GPIO pins
+    GPIO.setup(direction_pin, GPIO.OUT)
+    GPIO.setup(step_pin, GPIO.OUT)
+    GPIO.output(direction_pin, direction)
+
+    # Run the motor
+    try:
+
+        # Have the motor wait before starting
+        time.sleep(initialdelay)
+
+        # Loop through the steps
+        for i in range(steps):
+
+            # Stop the motor if the boolean variable is changed to true
+            if stop_motor:
+                break
+
+            # Otherwise run
+            else:
+                GPIO.output(step_pin, True)
+                time.sleep(stepdelay)
+                GPIO.output(step_pin, False)
+                time.sleep(stepdelay)
+
+    # Display an error
+    except Exception as motor_error:
+        print(sys.exc_info()[0])
+        print(motor_error)
+        print("RpiMotorLib  : Unexpected error:")
+
+    # Cleanup
+    finally:
+        GPIO.output(self.step_pin, False)
+        GPIO.output(self.direction_pin, False)
+        GPIO.cleanup()
--- src/motor_test.py
+++ src/motor_test.py
@@ -1,18 +1,3 @@
-import RPi.GPIO as GPIO
-import RpiMotorLib
-import time
+import motor_controls
 
-direction = 22
-step = 23
-EN_pin = 24
-
-mymotortest = RpiMotorLib.A4988Nema(direction, step, (21, 21, 21), "DRV8825")
-GPIO.setup(EN_pin, GPIO.OUT)
-
-GPIO.output(EN_pin, GPIO.LOW)
-mymotortest.motor_go(False, "1/16", 25600, 0.00000005, False, 0.05)
-
-GPIO.output(EN_pin, GPIO.LOW)
-mymotortest.motor_go(True, "1/16", 25600, 0.00000005, False, 0.05)
-
-GPIO.cleanup()
+motor_controls.auto_run(False, 6400, 0.00003, 0.05)

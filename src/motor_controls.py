805e084a11c58fb815fe27bae3c27c4a78d94028
Walker Allen <walker@a3z.co>
2024-07-06 11:25:39 -0400

    testing motor controls

--- src/motor_controls.py
+++ src/motor_controls.py
@@ -1,5 +1,9 @@
-# Set the file path for writing commands to the stepper motor
-file_path = "/tmp/printer"
+import serial
+import time
+
+# Set the serial port and baud rate for communcating with Klipper
+serial_port = "/tmp/printer"
+baud_rate = 250000
 
 # Create a function to send commands to the stepper motor
 def run_dip_coater(run_parameters):
@@ -12,22 +16,37 @@ def run_dip_coater(run_parameters):
 
 
     down_command = "G0 X" + str(down_position) + " F" + str(down_speed) + "\n"
-    dwell_command = "G4 P" + str(stay_time) + "\n"
+    dwell_command = "G4 P" + str(dwell_time) + "\n"
     up_command = "G0 X" + str(up_position) + " F" + str(up_speed) + "\n"
     upper_pause_command = "G4 P1000\n"
     commands = []
 
-    for dips in run_paremeters[6]:
+    for dips in run_parameters[6]:
         commands.append(down_command)
         commands.append(dwell_command)
         commands.append(up_command)
         commands.append(upper_pause_command)
     
-    commands.append("G28 X0")
+    commands.append("G28 X0\n")
     
-    with open (file_path, 'w') as file:
-        for command in commands:
-            file.write(command)
+    try:
+        with serial.Serial(serial_port, baud_rate, timeout=1) as ser:
+            print(f"Opened serial port: {serial_port}")
+
+            for command in commands:
+                # Send command
+                ser.write(command.encode())
+                print(f"Sent command: {command.strip()}")
+
+                # Read response (optional)
+                response = ser.readline().decode().strip()
+                print(f"Response from Klipper: {response}")
+
+            # Close the serial port
+            print("Serial port closed")
+
+    except serial.SerialException as e:
+        print(f"Error opening or communicating with {serial_port}: {e}")
 
 def get_run_duration(run_parameters):
 
--- src/test_file.py
+++ src/test_file.py
@@ -1,30 +1,5 @@
-import serial
-import time
+import motor_controls
 
-serial_port = "/tmp/printer"
-baud_rate = 250000
+list = [80, 100, 65, 20, 10, 3, 1]
 
-commands = [
-    "G0 X40 F600\n",
-    "G4 P3000\n"
-    "G0 X0 F600\n"
-]
-
-try:
-    ser = serial.Serial(serial_port, baud_rate, timeout=1)
-    print(f"Opened serial port: {serial_port}")
-
-    for command in commands:
-        # Send command
-        ser.write(command.encode())
-
-        # Read response (optional)
-        response = ser.readline().decode().strip()
-        print(f"Response from Klipper: {response}")
-
-    # Close the serial port
-    ser.close()
-    print("Serial port closed")
-
-except serial.SerialException as e:
-    print(f"Error opening or communicating with {serial_port}: {e}")
+motor_controls.run_dip_coater(list)

6d3b4ce080781110057e867fb6405445293e8d45
Walker Allen <walker@a3z.co>
2024-07-06 11:10:20 -0400

    multiple commands

--- src/test_file.py
+++ src/test_file.py
@@ -4,15 +4,19 @@ import time
 serial_port = "/tmp/printer"
 baud_rate = 250000
 
+commands [
+    "G0 X40 F600\n",
+    "G4 P3000\n"
+    "G0 X0 F600\n"
+]
+
 try:
     ser = serial.Serial(serial_port, baud_rate, timeout=1)
     print(f"Opened serial port: {serial_port}")
 
-    # Example command to send to Klipper
-    command = "G28 X0"
-
-    # Send command
-    ser.write(command.encode())
+    for command in commands:
+        # Send command
+        ser.write(command.encode())
 
     # Read response (optional)
     #response = ser.readline().decode().strip()

eb0690da9bb1c18a269dd39956cfd0b2a82dd960
Walker Allen <walker@a3z.co>
2024-07-12 22:09:04 -0400

    Now starting timeout using dedicated function

--- src/dip_coater_interface.py
+++ src/dip_coater_interface.py
@@ -35,6 +35,13 @@ c.execute("""CREATE TABLE savedruns (
 ###
 
 
+# Create a function to start the power off timeout
+def start_timeout():
+    global backlight_poweroff
+
+    time.sleep(1)
+    backlight_poweroff = root.after(600000, power_off_backlight)
+
 # Create a power state variable
 power_state = 0
 
@@ -788,9 +795,7 @@ root.bind('<KP_6>', reset_poweroff)
 root.bind('<KP_7>', reset_poweroff)
 root.bind('<KP_8>', reset_poweroff)
 root.bind('<KP_9>', reset_poweroff)
-backlight_poweroff = root.after(600000, power_off_backlight)
-# print("setting timeout")
 
-reset_poweroff()
+start_timeout()
 
 root.mainloop()
